a2aee63d0186fddde06db0ccc5f4ea47ae94e7c2
Pmilicaa <micy_98@live.com>
2021-08-19 15:22:41 +0200

    Added UnitOfMeasure class members

--- SalesSubsystem/src/main/java/com/salesSubsystem/model/UnitOfMeasure.java
+++ SalesSubsystem/src/main/java/com/salesSubsystem/model/UnitOfMeasure.java
@@ -2,4 +2,37 @@ package com.salesSubsystem.model;
 
 public class UnitOfMeasure {
 
+    private long id;
+    private String name;
+    private String shortName;
+
+    public UnitOfMeasure(long id, String name, String shortName) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.shortName = shortName;
+    }
+
+    public UnitOfMeasure() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getShortName() {
+        return shortName;
+    }
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 }
