b5e0d50b2771bf44e07daf7d3d3337d4addb44d2
Pmilicaa <micy_98@live.com>
2021-08-19 14:14:29 +0200

    Initial commit

---
+++ SalesSubsystem/src/main/java/com/salesSubsystem/SalesSubsystemApplication.java
@@ -0,0 +1,13 @@
+package com.salesSubsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSubsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSubsystemApplication.class, args);
+	}
+
+}
---
+++ SalesSubsystem/src/test/java/com/salesSubsystem/SalesSubsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesSubsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSubsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
