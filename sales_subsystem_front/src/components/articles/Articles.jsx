bd1311d1835099a8917ba0c3397006303ec079e2
Pmilicaa <micy_98@live.com>
2022-02-19 16:28:44 +0100

    Added modal for adding price and addPriceList method

--- sales_subsystem_front/src/components/articles/Articles.jsx
+++ sales_subsystem_front/src/components/articles/Articles.jsx
@@ -1,32 +1,37 @@
 import { useState } from "react";
-import { Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import ArticleGroup from "../articleGroups/ArticleGroup";
+import PriceList from "../priceList/PriceList";
 
 import './Articles.css';
-const Articles = ({ articles,articleGroups, companies }) => {
-    console.log(articles)
+const Articles = ({ articles, articleGroups, companies }) => {
     let navigate = useNavigate();
-
-    const getPrice = (article)=>{
+    const [show, setShow] = useState(false);
+    const getPrice = (article) => {
         let price;
-        if(article.priceLists.length === 0){
+        if (article.priceLists.length === 0) {
             return 'no price';
         }
         for (let index = 0; index < article.priceLists.length; index++) {
-             price = article.priceLists[index].pricePerUnit;
+            price = article.priceLists[index].pricePerUnit;
         }
         return price;
     }
-    const handleClick = ()=>{
+    const handleClick = () => {
         console.log('usao')
         navigate('/addArticle', { replace: true });
     }
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        setShow(true); ;
+    }
+
     return (
         <>
-         <h1 className="center">Articles</h1>
-           {articles && <Table className="table" striped bordered hover>
+            <h1 className="center">Articles</h1>
+            {articles && <Table className="table" striped bordered hover>
                 <thead className="thead-dark">
                     <tr>
                         <th scope="col">Name</th>
@@ -38,21 +43,40 @@ const Articles = ({ articles,articleGroups, companies }) => {
                     </tr>
                 </thead>
                 <tbody>
-                   {articles.map((article)=>
-                    <tr>
-                        <td>{article.name}</td>
-                        <td>{article.description}</td>
-                        <td>{getPrice(article)}</td>
-                       {article.unitOfMeasure.shortName && <td>{article.unitOfMeasure.shortName}</td>}
-                        <td>{article.articleGroup.name}</td>
-                        <td>{article.company.pib}</td>
+                    {articles.map((article) =>
+                        <tr>
+                            <td>{article.name}</td>
+                            <td>{article.description}</td>
+                            <td>{getPrice(article)}</td>
+                            {article.unitOfMeasure.shortName && <td>{article.unitOfMeasure.shortName}</td>}
+                            <td>{article.articleGroup.name}</td>
+                            <td>{article.company.pib}</td>
+                            <Button type="submit" onClick={handleShow} >Edit</Button>
 
-                    </tr>)}
+                        </tr>)}
                 </tbody>
             </Table>}
-           <div className="component"> <button type="submit" className="btn-primary" onClick={handleClick}>Add article</button></div>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add new price</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <PriceList  />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <div className="component"> <button type="submit" className="btn-primary" onClick={handleClick}>Add article</button></div>
 
-           <ArticleGroup articleGroups={articleGroups} companies={companies}/>
+            <ArticleGroup articleGroups={articleGroups} companies={companies} />
         </>
     )
 }
